2a21c9f788ff6596bd3b23501698c3d801c20dd1
Ryan <hiRyan@protonmail.com>
2019-02-24 17:38:20 +0200

    basic gradle structure created

---
+++ src/main/java/hansabgroup/hansabmotorsgradle/HansabmotorsgradleApplication.java
@@ -0,0 +1,13 @@
+package hansabgroup.hansabmotorsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansabmotorsgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansabmotorsgradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hansabgroup/hansabmotorsgradle/HansabmotorsgradleApplicationTests.java
@@ -0,0 +1,16 @@
+package hansabgroup.hansabmotorsgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HansabmotorsgradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
